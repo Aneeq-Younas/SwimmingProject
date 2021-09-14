8b01ce420351b78abcca24a74c86619f2aa8d885
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:08:57 -0700

    Add files via upload

---
+++ swim.py
@@ -0,0 +1,36 @@
+print("What is your age? ")
+
+age= int (input())
+
+if age<5:
+    print("You can not swim")
+
+elif age<=5:
+    print("You are just a five years old child")
+
+elif age==6:
+    print("We will think about you ")
+
+
+elif age==18:
+    print("You can swim, on one side of pool where the pool height is 4 feet ")
+
+elif age>=19:
+    print("You can swim  ")
+
+elif age==40:
+    print("You are 40 years old you can swim your own responsibility  ")
+
+else:
+    print("your age is over you can not swim ")
+
+
+
+
+
+
+SwimmingProject
+
+Enter your age and the program will tell you that you can swim or not according to your age
+
+
